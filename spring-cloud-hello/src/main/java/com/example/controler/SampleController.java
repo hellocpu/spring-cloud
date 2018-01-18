6bb8945329e6b8e70cb81c73e2d9297787a08687
wangt <wangt@bsfit.com.cn>
2018-01-18 16:59:35 +0800

    hello

---
+++ spring-cloud-hello/src/main/java/com/example/controler/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+	@ResponseBody
+	@RequestMapping("/hello")
+	String index() {
+		return "hello,world!";
+	}
+}
