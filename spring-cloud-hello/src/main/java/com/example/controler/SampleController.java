ca5d71a9dc1102491e3f5b3d185fbf93d33a7563
wangt <wangt@bsfit.com.cn>
2018-03-29 10:41:31 +0800

    test

--- spring-cloud-hello/src/main/java/com/example/controler/SampleController.java
+++ spring-cloud-hello/src/main/java/com/example/controler/SampleController.java
@@ -1,14 +1,24 @@
 package com.example.controler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RefreshScope
 public class SampleController {
+	@Autowired
+	private Info info;
+	
+	@Value("${mysqldb.datasource.username}")
+	private String username;
+	
 	@ResponseBody
 	@RequestMapping("/hello")
 	String index() {
-		return "hello,world!";
+		return "hello,"+ username;
 	}
 }
