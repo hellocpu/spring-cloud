2a4b0d61dbdae928c6947e55f8f674a7f2c5ca7f
wangt <wangt@bsfit.com.cn>
2018-03-29 17:00:26 +0800

    test

--- spring-cloud-hello/src/main/java/com/example/SpringCloudApplication.java
+++ spring-cloud-hello/src/main/java/com/example/SpringCloudApplication.java
@@ -2,8 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//@EnableDiscoveryClient // 激活eureka中的DiscoveryClient实现
+@EnableEurekaClient
 public class SpringCloudApplication {
 
 	public static void main(String[] args) {
--- spring-cloud-hello/src/main/java/com/example/controler/SampleController.java
+++ spring-cloud-hello/src/main/java/com/example/controler/SampleController.java
@@ -1,7 +1,12 @@
 package com.example.controler;
 
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +17,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SampleController {
 	@Autowired
 	private Info info;
-	
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
 	@Value("${mysqldb.datasource.username}")
 	private String username;
-	
+
 	@Value("${redis.uri}")
 	private String redisPort;
-	
+
 	@ResponseBody
 	@RequestMapping("/hello")
 	String index() {
-		return "hello,"+ username +":" + redisPort;
+		StringBuilder buf = new StringBuilder();
+		List<String> serviceIds = discoveryClient.getServices();
+		if (!CollectionUtils.isEmpty(serviceIds)) {
+			for (String s : serviceIds) {
+				System.out.println("serviceId:" + s);
+				List<ServiceInstance> serviceInstances = discoveryClient.getInstances(s);
+				if (!CollectionUtils.isEmpty(serviceInstances)) {
+					for (ServiceInstance si : serviceInstances) {
+						buf.append("[" + si.getServiceId() + " host=" + si.getHost() + " port=" + si.getPort() + " uri="
+								+ si.getUri() + "]");
+					}
+				} else {
+					buf.append("no service.");
+				}
+			}
+		}
+
+		return buf.toString();
 	}
 }
