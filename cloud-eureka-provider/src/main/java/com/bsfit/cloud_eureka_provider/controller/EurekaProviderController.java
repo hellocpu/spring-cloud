7fd0dc46db3b41d601d1416c9d3dd18209d01886
wangt <wangt@bsfit.com.cn>
2018-03-30 10:24:07 +0800

    test

--- cloud-eureka-provider/src/main/java/com/bsfit/cloud_eureka_provider/controller/EurekaProviderController.java
+++ cloud-eureka-provider/src/main/java/com/bsfit/cloud_eureka_provider/controller/EurekaProviderController.java
@@ -1,14 +1,19 @@
 package com.bsfit.cloud_eureka_provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EurekaProviderController {
+
+	@Value("${server.port}")
+	private String port;
+
 	@RequestMapping("/eureka/provider")
 	@ResponseBody
-	public String test(){
-		return "eureka provider";
+	public String test() {
+		return "current port :" + port;
 	}
 }
